0d0ee2e70ffcde276d575a1297911f981dd99781
Marco Spanghero <marcosp@kth.se>
2020-06-02 08:57:11 +0200

    everything works fine -- just need to fix the signing

--- __init__.py
+++ __init__.py
@@ -10,9 +10,12 @@ import itertools
 import sys
 import hashlib
 import base64
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 
 from struct import *
 
@@ -23,19 +26,68 @@ except ModuleNotFoundError:
     warn("sha3 is not working!")
 
 
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+
+
 class secMerkleTools(object):
-    def __init__(self, hash_type="sha256", isSecure=False, key=None, doSign=False):
+    def __init__(self, hash_type="sha256", isSecure=False, doSign=False, path='config.ini'):
+
         hash_type = hash_type.lower()
         self.secureTree = isSecure
         self.doSign = doSign
         if self.secureTree:
-            if key is None:
-                print('Key not specified, generating a random 128 bit KEY:')
-                self.key = os.urandom(16)
-                print('Key is : {}'.format(binascii.hexlify(self.key)))
-            else:
-                self.key = key
-                print('Using supplied key RSA')
+            '''Setup configuration for the sender. Configurations are loaded from the transmitter.ini file'''
+            config = configparser.RawConfigParser()
+            config.read(path)
+
+            '''Generate public/private key pair'''
+            '''Check if tranmsitter.ini has any key available'''
+            priv_key_path = config.get('keys', 'priv_key_path')
+            pub_key_path = config.get('keys', 'pub_key_path')
+            try:
+                with open(priv_key_path, "rb") as key_file:
+                    self.private_key = serialization.load_pem_private_key(
+                        key_file.read(),
+                        password=None,
+                        backend=default_backend()
+                    )
+
+                with open(pub_key_path, "rb") as key_file:
+                    self.public_key = serialization.load_pem_public_key(
+                        key_file.read(),
+                        backend=default_backend()
+                    )
+                print('Found available keys')
+            except Exception as e:
+                print('Exception {}: Key not found, generating a new public/private key pair to be stored'.format(e))
+                private_key = rsa.generate_private_key(
+                    public_exponent=65537,
+                    key_size=2048,
+                    backend=default_backend()
+                )
+                public_key = private_key.public_key()
+
+                private_key = private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.NoEncryption()
+                )
+
+                with open('private_key.pem', 'wb') as f:
+                    f.write(private_key)
+
+                public_key = public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo
+                )
+
+                with open('public_key.pem', 'wb') as f:
+                    f.write(public_key)
+
             if hash_type in ['sha256', 'md5', 'sha224', 'sha384', 'sha512',
                              'sha3_256', 'sha3_224', 'sha3_384', 'sha3_512']:
                 self.digestmod = hash_type
@@ -159,12 +211,13 @@ class secMerkleTools(object):
     def _calculate_signatures(self):
         N = int(self.get_levels_count())
         for n in tqdm(range(0, N)):
-            signature = self.key.sign(
-                binascii.hexlify(self.levels[n]),
+            signature = self.private_key.sign(
+                self.levels[n],
                     padding.PSS(
                     mgf=padding.MGF1(hashes.SHA256()),
                     salt_length=padding.PSS.MAX_LENGTH),
                     hashes.SHA256())
+            print("\t Level: {} {} \n Signa: {}".format(self.levels[n], binascii.hexlify(self.levels[n]), signature))
             self.sig_levels.append(signature)
 
     def _calculate_secure_leaves(self, rotate_frequency):
